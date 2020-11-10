1888cb86070168f0ec1f20e54517bd34143e969b
Test <50669432+testdummycloud@users.noreply.github.com>
2020-11-10 19:38:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http')
 const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
-    console.log('New connection-ci-cd')
-    res.end('Hello Cloud Run with CI/CD')
+    console.log('New connection-ci-cd - revision 2')
+    res.end('Hello Cloud Run with CI/CD - revision 2')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
